e79e55cf6088c6fbd5b376105d1ea0f18e9e64cb
Azeem yoonus <azeemyoonus4@gmail.com>
2021-01-05 13:39:33 +0530

    added practice project (strong password) in chapter 7

---
+++ Chapter-07/Strong_Password_Detection.py
@@ -0,0 +1,21 @@
+import re
+
+char_10=re.compile(r'.{8,}')
+upper_char=re.compile(r'[A-Z]')
+lower_char=re.compile(r'[a-z]')
+digit=re.compile(r'[0-9]')
+
+password=str(input("Enter a password to check wether it is strong or not:"))
+
+charcheck=char_10.search(password)
+uppercheck=upper_char.search(password)
+lowercheck=lower_char.search(password)
+digitcheck=digit.search(password)
+
+
+
+if (charcheck and uppercheck and lowercheck and digitcheck):
+    print("Your password is strong")
+else:
+    print("Your password is not strong. Try to change")
+    
